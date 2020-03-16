d8f232935bea9d15f6be3564aee1bb0aa8800943
Your Name <you@example.com>
2020-03-16 08:31:52 -0400

    adding files

---
+++ Count.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Count
+{public static void main(String[] args)
+{int i;
+{for (i=1; i<=4; i++ )
+System.out.println(2+" times "+i+" = "+(2*i));
+}
+System.out.println("Hello!");
+}
+}
---
+++ Makefile
@@ -0,0 +1,8 @@
+all:
+	echo "Use 'build' to compile, and 'doit' to run file."
+build:
+	javac Count.java
+doit:
+	java Count
+
+
